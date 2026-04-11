0b04887087cfb13d02ba300f65f7491233ead5a1
jmb <juanmiguelluna2004@gmail.com>
2026-04-11 13:13:33 +0800

    small fix
    made swap code cleaner and reusable

--- tps/scenes/camera.gd
+++ tps/scenes/camera.gd
@@ -16,6 +16,9 @@ var mouse_sensitivity: float = 0.001
 var max_y_rotation: float = 1.2
 var camera_tween: Tween
 
+enum CameraAlignment {LEFT = -1, RIGHT = 1, CENTRE = 0}
+var current_camera_alignment: int = CameraAlignment.RIGHT
+
 @onready var default_edge_spring_arm_length: float = edge_spring_arm.spring_length
 @onready var default_rear_spring_arm_length: float = rear_spring_arm.spring_length
 @onready var default_fov: float = camera.fov
@@ -53,8 +56,20 @@ func camera_look(mouse_movement: Vector2) -> void:
 	camera_rotation.y = clamp(camera_rotation.y, -max_y_rotation, max_y_rotation)
 	
 func swap_camera_alignment() -> void:
-	var new_pos: float = default_edge_spring_arm_length * -sign(edge_spring_arm.spring_length)
+	match current_camera_alignment:
+		
+		CameraAlignment.RIGHT:
+			set_current_camera_alignment(CameraAlignment.LEFT)
+		CameraAlignment.LEFT:
+			set_current_camera_alignment(CameraAlignment.RIGHT)
+		CameraAlignment.CENTRE:
+			return
+			
+	var new_pos: float = default_edge_spring_arm_length * current_camera_alignment
 	set_rear_spring_arm_position(new_pos, camera_alignment_speed)
+
+func set_current_camera_alignment(alignment: CameraAlignment) -> void:
+	current_camera_alignment = alignment
 	
 func set_rear_spring_arm_position(pos: float, speed: float) -> void:
 	if camera_tween:
@@ -71,7 +86,7 @@ func enter_aim() -> void:
 	camera_tween.set_parallel()
 	
 	camera_tween.tween_property(camera, "fov", aim_fov, aim_speed)
-	camera_tween.tween_property(edge_spring_arm, "spring_length", aim_edge_spring_length * sign(edge_spring_arm.spring_length), aim_speed)
+	camera_tween.tween_property(edge_spring_arm, "spring_length", aim_edge_spring_length * current_camera_alignment, aim_speed)
 	camera_tween.tween_property(rear_spring_arm, "spring_length", aim_rear_spring_length, aim_speed)
 
 func exit_aim() -> void:
@@ -82,5 +97,5 @@ func exit_aim() -> void:
 	camera_tween.set_parallel()
 
 	camera_tween.tween_property(camera, "fov", default_fov, aim_speed)
-	camera_tween.tween_property(edge_spring_arm, "spring_length", default_edge_spring_arm_length * sign(edge_spring_arm.spring_length), aim_speed)
+	camera_tween.tween_property(edge_spring_arm, "spring_length", default_edge_spring_arm_length * current_camera_alignment, aim_speed)
 	camera_tween.tween_property(rear_spring_arm, "spring_length", default_rear_spring_arm_length, aim_speed)
